5387f1d29eb033a5197306e7393a9b314be6f421
Sofia Gadea <slgadea@miuandes.cl>
2021-03-22 17:53:45 -0300

    third commmit

--- Parte1.py
+++ Parte1.py
@@ -1,18 +1,16 @@
 import random
 cantidad_cartas = int(input("Diga la cantidad de cartas a jugar"))
 cartas = []
-for i in range(1,cantidad_cartas+1):
-    a = random.randint(1,cantidad_cartas)
-    while a in cartas:
-        a = random.randint(1,cantidad_cartas)
-    cartas.append(a)
 cartas2 = []
 for i in range(1,cantidad_cartas+1):
     a = random.randint(1,cantidad_cartas)
-    while a in cartas2:
+    while a in cartas or a in cartas2:
         a = random.randint(1,cantidad_cartas)
+    cartas.append(a)  
     cartas2.append(a)
-for i in cartas2:
-    cartas.append(i)
+for i in range(0,cantidad_cartas):
+    cartas.insert(random.randint(0,cantidad_cartas), cartas2[i])
+print("*",cartas,"*")
+
 jugador_1 = 0
 jugador_2 = 0
