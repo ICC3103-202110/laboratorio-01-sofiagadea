a1760bb85997af726acf3b21d23d6aa127fc7b30
Sofia Gadea <slgadea@miuandes.cl>
2021-03-22 13:31:15 -0300

    second commit

--- Parte1.py
+++ Parte1.py
@@ -6,6 +6,13 @@ for i in range(1,cantidad_cartas+1):
     while a in cartas:
         a = random.randint(1,cantidad_cartas)
     cartas.append(a)
+cartas2 = []
+for i in range(1,cantidad_cartas+1):
+    a = random.randint(1,cantidad_cartas)
+    while a in cartas2:
+        a = random.randint(1,cantidad_cartas)
+    cartas2.append(a)
+for i in cartas2:
+    cartas.append(i)
 jugador_1 = 0
 jugador_2 = 0
-print(cartas)
